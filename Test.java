7d5c648021da418d2d935d80048e4e40e371e902
nisha-km <78420079+nisha-km@users.noreply.github.com>
2021-02-10 16:10:47 +0530

    Assignment comfile

---
+++ Test.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import com.shape.Rectangle;
+import com.shape.Square;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Square square = new Square();
+		square.calcArea();
+		square.calcPeri();
+		Rectangle rectangle= new Rectangle();
+		rectangle.calcArea();
+		rectangle.calcPeri();
+
+	}
+
+}
+  
